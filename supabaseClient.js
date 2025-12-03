0e8ed31a03abc3ec2de2ab344f125739bad10623
tai928 <taisei.study928@gmail.com>
2025-12-03 22:12:33 +0900

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
 const SUPABASE_URL = "https://ngtthuwmqdcxgddlbsyo.supabase.co";
-const SUPABASE_KEY = "YOUR_PUBLIC_ANON_KEY";
+const SUPABASE_KEY = "sb_publishable_YJzguO8nmmVKURa58cKwVw__9ulKxI6";
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
